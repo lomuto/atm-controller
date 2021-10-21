c2603e2861aa0e6f45e14eb7e53191e78d0b0f4b
lomuto <kangys1245@gmail.com>
2021-10-21 17:00:52 +0900

    Init Account class

---
+++ src/com/lomuto/Account.java
@@ -0,0 +1,27 @@
+package com.lomuto;
+
+public class Account {
+    private final String accountNumber;
+    private int balance;
+
+    public Account(String accountNumber, int balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        this.balance -= amount;
+    }
+}
