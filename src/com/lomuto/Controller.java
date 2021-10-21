a4d7c93ebb947d1cf77e53a2d3fce6e899a39bbe
lomuto <kangys1245@gmail.com>
2021-10-21 17:02:20 +0900

    Init Controller class

---
+++ src/com/lomuto/Controller.java
@@ -0,0 +1,44 @@
+package com.lomuto;
+
+public class Controller {
+    private static final int MAX_CASH = 1000;
+    private int remainCash;
+    private static Controller controllerInstance;
+
+    public static Controller getInstance() {
+        if(controllerInstance == null) {
+            controllerInstance = new Controller();
+        }
+
+        return controllerInstance;
+    }
+
+    public boolean setRemainCash(int amount) {
+        if(amount < 0) {
+            return false;
+        }
+
+        this.remainCash = amount;
+        return true;
+    }
+
+    public boolean depositCash(int amount) {
+        if(this.remainCash + amount > MAX_CASH) {
+            return false;
+        }
+
+        this.remainCash += amount;
+        return true;
+    }
+
+    public boolean withdrawCash(int amount) {
+        if(this.remainCash - amount < 0) {
+            return false;
+        }
+
+        this.remainCash -= amount;
+        return true;
+    }
+
+    private Controller(){}
+}
