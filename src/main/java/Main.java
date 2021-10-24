926087eb152db04b7bb790d98fa634016085eb0d
lomuto <kangys1245@gmail.com>
2021-10-24 14:15:52 +0900

    Update main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class Main {
+    private static Thread getThread(ATMMachine atmMachine) {
+        return new Thread(() -> {
+            System.out.printf("%s Thread starts%s", atmMachine.getClass().getName(), System.lineSeparator());
+            while(true) {
+                // waiting for stream
+                ;
+            }
+        });
+    }
+    public static void main(String[] args) {
+        ATMMachine atm0 = new ATMMachine();
+        ATMMachine atm1 = new ATMMachine();
+        ATMMachine atm2 = new ATMMachine();
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
+        threadPool.submit(getThread(atm0));
+        threadPool.submit(getThread(atm1));
+        threadPool.submit(getThread(atm2));
     }
 }
